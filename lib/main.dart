085216391e59be4e76d321c17707b3a2bdfee18c
dariowskii <dariovarriale@rocketmail.com>
2026-03-05 00:02:19 +0100

    fix: wrap MyApp in ProviderScope for Riverpod integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_app/features/home_feature/presentation/home_page.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(
+  const ProviderScope(
+    child: MyApp(),
+  ),
+);
 
 /// The main app widget.
 class MyApp extends StatelessWidget {
