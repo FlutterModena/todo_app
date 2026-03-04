9cab56554c413070753f6c04246520741e60456c
dariowskii <dariovarriale@rocketmail.com>
2026-03-05 00:23:07 +0100

    feat: add providers for adding, updating, and deleting todos in todo provider

--- lib/features/home_feature/data/todo_provider.dart
+++ lib/features/home_feature/data/todo_provider.dart
@@ -15,3 +15,42 @@ Future<List<TodoModel>> todoList(Ref ref) async {
     () => todos,
   );
 }
+
+/// A provider that fetches the list of todos filtered by category.
+@riverpod
+Future<List<TodoModel>> filteredTodoList(
+  Ref ref, {
+  TodoCategory? category,
+}) async {
+  final todos = await ref.watch(todoListProvider.future);
+  if (category == null) {
+    return todos;
+  }
+
+  return todos.where((todo) => todo.category == category).toList();
+}
+
+/// A provider that adds a new todo to the list.
+@riverpod
+void addTodo(Ref ref, TodoModel todo) {
+  final lastTodo = todos.isNotEmpty ? todos.last : null;
+  final newId = (lastTodo?.id ?? 0) + 1;
+  final newTodo = todo.copyWith(id: newId);
+
+  todos.add(newTodo);
+}
+
+/// A provider that updates an existing todo in the list.
+@riverpod
+void updateTodo(Ref ref, TodoModel updatedTodo) {
+  final index = todos.indexWhere((todo) => todo.id == updatedTodo.id);
+  if (index != -1) {
+    todos[index] = updatedTodo;
+  }
+}
+
+/// A provider that deletes a todo from the list.
+@riverpod
+void deleteTodo(Ref ref, int id) {
+  todos.removeWhere((todo) => todo.id == id);
+}
