e3fb57e99561d451590c8711085fbe13c41d8616
dariowskii <dariovarriale@rocketmail.com>
2026-03-04 17:50:04 +0100

    feat: add isToday and isTomorrow getters to DateTimeExtension

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -8,4 +8,15 @@ extension ContextExtension on BuildContext {
 
 extension DateTimeExtension on DateTime {
   String format(String format) => DateFormat(format).format(this);
+  bool get isToday {
+    final now = DateTime.now();
+    return year == now.year && month == now.month && day == now.day;
+  }
+
+  bool get isTomorrow {
+    final tomorrow = DateTime.now().add(Duration(days: 1));
+    return year == tomorrow.year &&
+        month == tomorrow.month &&
+        day == tomorrow.day;
+  }
 }
