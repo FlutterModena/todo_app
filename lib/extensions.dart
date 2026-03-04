5d3ff7fcd5fc80e62fd48fdcf4e31cd0cbe25e31
dariowskii <dariovarriale@rocketmail.com>
2026-03-04 15:02:13 +0100

    feat: add intl package for date formatting and enhance DateTime extension

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 extension ContextExtension on BuildContext {
   TextTheme get textTheme => Theme.of(this).textTheme;
+}
+
+extension DateTimeExtension on DateTime {
+  String format(String format) => DateFormat(format).format(this);
 }
